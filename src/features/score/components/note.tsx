8ae2c4f3fe4f68be4f2f680730acc18821ceb780
Archie G <acgelonga@gmail.com>
2025-03-06 23:05:48 -0600

    position keys on g clef

--- src/features/score/components/note.tsx
+++ src/features/score/components/note.tsx
@@ -22,6 +22,64 @@ const assets = {
 };
 
 const positionMap = {
+  a0: 25,
+  b0: 24,
+  c1: 23,
+  d1: 22,
+  e1: 21,
+  f1: 20,
+  g1: 19,
+  a1: 18,
+  b1: 17,
+  c2: 16,
+  d2: 15,
+  e2: 14,
+  f2: 13,
+  g2: 12,
+  a2: 11,
+  b2: 10,
+  c3: 9,
+  d3: 8,
+  e3: 7,
+  f3: 6,
+  g3: 5,
+  a3: 4,
+  b3: 3,
+  c4: 2,
+  d4: 1,
+  e4: 0,
+  f4: -1,
+  g4: -2,
+  a4: -3,
+  b4: -4,
+  c5: -5,
+  d5: -6,
+  e5: -7,
+  f5: -8,
+  g5: -9,
+  a5: -10,
+  b5: -11,
+  c6: -12,
+  d6: -13,
+  e6: -14,
+  f6: -15,
+  g6: -16,
+  a6: -17,
+  b6: -18,
+  c7: -19,
+  d7: -20,
+  e7: -21,
+  f7: -22,
+  g7: -23,
+  a7: -24,
+  b7: -25,
+  c8: -26
+};
+
+const INCREMENT = 11;
+const BASE_POS = 88;
+
+const NOTE_POSITION_OFFSET = {
   a0: '',
   b0: '',
   c1: '',
@@ -45,35 +103,8 @@ const positionMap = {
   g3: '',
   a3: '',
   b3: '',
-  c4: '',
-  d4: '',
-  e4: 88,
-  f4: 77,
-  g4: 66,
-  a4: 55,
-  b4: 44,
-  c5: 33,
-  d5: 22,
-  e5: 11,
-  f5: 0,
-  g5: -11,
-  a5: -22,
-  b5: -33,
-  c6: -44,
-  d6: -55,
-  e6: -66,
-  f6: -77,
-  g6: -88,
-  a6: -99,
-  b6: -110,
-  c7: -121,
-  d7: -132,
-  e7: -143,
-  f7: -154,
-  g7: -165,
-  a7: -176,
-  b7: -187,
-  c8: -198
+  c4: -2,
+  d4: -1
 };
 
 const NoteContainer = chakra('div', {
@@ -85,13 +116,25 @@ const NoteContainer = chakra('div', {
   }
 });
 
+const NoteName = chakra('span', {
+  base: {
+    position: 'absolute',
+    color: '#000',
+    left: -2,
+    bottom: 2,
+    fontSize: 12
+  }
+});
+
 export function Note({ tonality, duration }: Props) {
+  const top =
+    typeof positionMap[tonality] !== 'undefined'
+      ? `${positionMap[tonality] * INCREMENT + BASE_POS}px`
+      : 0;
+  console.log(tonality, top);
   return (
-    <NoteContainer
-      data-component="note"
-      data-key={`${tonality}`}
-      top={positionMap[tonality] ? `${positionMap[tonality]}px` : 0}
-    >
+    <NoteContainer data-component="note" data-key={`${tonality}`} top={top}>
+      <NoteName>{tonality}</NoteName>
       <Image src={assets[duration]} alt="" />
     </NoteContainer>
   );
--- src/features/score/pages/create.tsx
+++ src/features/score/pages/create.tsx
@@ -4,6 +4,8 @@ import { Beat, Note, Row } from '@/features/score';
 
 type Props = {};
 
+const DURATION = 1;
+
 function ScoreCreate({}: Props) {
   return (
     <Box paddingTop={40}>
@@ -11,165 +13,165 @@ function ScoreCreate({}: Props) {
         <Card.Body>
           <Row>
             <Beat>
-              <Note tonality="a0" duration={4} />
+              <Note tonality="a0" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="b0" duration={4} />
+              <Note tonality="b0" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="c1" duration={4} />
+              <Note tonality="c1" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="d1" duration={4} />
+              <Note tonality="d1" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="e1" duration={4} />
+              <Note tonality="e1" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="f1" duration={4} />
+              <Note tonality="f1" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="g1" duration={4} />
+              <Note tonality="g1" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="a1" duration={4} />
+              <Note tonality="a1" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="b1" duration={4} />
+              <Note tonality="b1" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="c2" duration={4} />
+              <Note tonality="c2" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="d2" duration={4} />
+              <Note tonality="d2" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="e2" duration={4} />
+              <Note tonality="e2" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="f2" duration={4} />
+              <Note tonality="f2" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="g2" duration={4} />
+              <Note tonality="g2" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="a2" duration={4} />
+              <Note tonality="a2" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="b2" duration={4} />
+              <Note tonality="b2" duration={DURATION} />
             </Beat>
           </Row>
 
           <Row>
             <Beat>
-              <Note tonality="c3" duration={4} />
+              <Note tonality="c3" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="d3" duration={4} />
+              <Note tonality="d3" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="e3" duration={4} />
+              <Note tonality="e3" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="f3" duration={4} />
+              <Note tonality="f3" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="g3" duration={4} />
+              <Note tonality="g3" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="a3" duration={4} />
+              <Note tonality="a3" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="b3" duration={4} />
+              <Note tonality="b3" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="c4" duration={4} />
+              <Note tonality="c4" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="d4" duration={4} />
+              <Note tonality="d4" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="e4" duration={4} />
+              <Note tonality="e4" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="f4" duration={4} />
+              <Note tonality="f4" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="g4" duration={4} />
+              <Note tonality="g4" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="a4" duration={4} />
+              <Note tonality="a4" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="b4" duration={4} />
+              <Note tonality="b4" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="c5" duration={4} />
+              <Note tonality="c5" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="d5" duration={4} />
+              <Note tonality="d5" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="e5" duration={4} />
+              <Note tonality="e5" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="f5" duration={4} />
+              <Note tonality="f5" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="g5" duration={4} />
+              <Note tonality="g5" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="a5" duration={4} />
+              <Note tonality="a5" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="b5" duration={4} />
+              <Note tonality="b5" duration={DURATION} />
             </Beat>
           </Row>
           <Row>
             <Beat>
-              <Note tonality="c6" duration={4} />
+              <Note tonality="c6" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="d6" duration={4} />
+              <Note tonality="d6" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="e6" duration={4} />
+              <Note tonality="e6" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="f6" duration={4} />
+              <Note tonality="f6" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="g6" duration={4} />
+              <Note tonality="g6" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="a6" duration={4} />
+              <Note tonality="a6" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="b6" duration={4} />
+              <Note tonality="b6" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="c7" duration={4} />
+              <Note tonality="c7" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="d7" duration={4} />
+              <Note tonality="d7" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="e7" duration={4} />
+              <Note tonality="e7" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="f7" duration={4} />
+              <Note tonality="f7" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="g7" duration={4} />
+              <Note tonality="g7" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="a7" duration={4} />
+              <Note tonality="a7" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="b7" duration={4} />
+              <Note tonality="b7" duration={DURATION} />
             </Beat>
             <Beat>
-              <Note tonality="c8" duration={4} />
+              <Note tonality="c8" duration={DURATION} />
             </Beat>
           </Row>
         </Card.Body>
